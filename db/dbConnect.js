451deb78a8e0ff340e88ad535a24c482cdf483b0
rtokhirov <raxmatillotoxirov2003@gmail.com>
2023-07-09 00:38:23 +0500

    changed

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,9 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("countries","postgres","1202",{
-    host:"localhost",
-    dialect:"postgres"
-})
+// const db = new Sequelize("countries","postgres","1202",{
+//     host:"localhost",
+//     dialect:"postgres"
+// })
+
+const db= new Sequelize(
+    "postgres://countries_ja5y_user:S6eyJt9Yo8fx9hviFfqhG2d8oOXluObb@dpg-cikri7p5rnuvtgra2prg-a/countries_ja5y"
+)
 
 try {
     await db.authenticate()
